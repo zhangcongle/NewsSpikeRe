99952e0f3f1bf286d5d05481859f25ffa24d8e30
zhangcongle <zhangcongle@gmail.com>
2015-07-07 13:00:53 -0700

    nsre2/run.sh

--- nsre2/run.sh
+++ nsre2/run.sh
@@ -4,4 +4,4 @@ CMD="java -Xmx10g -cp target/classes:target/lib/*"
 # $CMD edu.washington.cs.figer.FigerSystem data/a
 # $CMD edu.washington.nsre.figer.ParseStanfordFigerReverb data/preprocess/text data/preprocess/parsed
 $CMD edu.washington.nsre.figer.Parsed2Tuple data/preprocess/parsed data/preprocess/tuples
-# $CMD edu.washington.nsre.ilp.lo2
+$CMD edu.washington.nsre.ilp.lo2
